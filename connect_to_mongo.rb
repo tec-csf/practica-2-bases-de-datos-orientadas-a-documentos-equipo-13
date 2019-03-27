2bd8781218755dba6b3b6a0f86985669bd2051f3
Salo Charabati <salochara@gmail.com>
2019-03-26 23:27:37 -0600

    changed ip addres

--- connect_to_mongo.rb
+++ connect_to_mongo.rb
@@ -1,6 +1,6 @@
 require 'mongo'
 
-client = Mongo::Client.new([ '0.0.0.0:27017' ], :database => 'tarea', :ssl => false)
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'tarea', :ssl => false)
 db = client.database
 
 p db.collections
