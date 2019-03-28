11f849fa1997764aad9de06054fc9831e6f15f5f
Salo Charabati <salochara@gmail.com>
2019-03-27 20:10:25 -0600

    added the code for Flask app

--- flask-mongo/main.py
+++ flask-mongo/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 app = FlaskAPI(__name__)
-
+# Home page
 @app.route("/", methods=['GET'])
 def list():
     mongo_uri = "mongodb://mongo-router:27017"
@@ -23,6 +23,53 @@ def list():
         notes.append(note)
 
     return notes
+# Dispositivos page
+# Para mostrar los países en donde hay dispositivos de la marca Sony
+@app.route("/dispositivos", methods=['GET'])
+def list_dispositivos():
+    mongo_uri = "mongodb://mongo-router:27017"
+
+    client = MongoClient(mongo_uri)
+    db = client.tarea
+    collection = db.dispositivos
+
+    pipeline = [{$match:{"marca":"Sony"}},{$project:{"marca":1,"pais":1, "_id":0}}, {$sort:{"_id":1}}]
+
+    cursor = collection.aggregate(pipeline)
+
+    return cursor
+# Direccion page
+# # Regresa la cantidad de dispositivos en México
+# El $count es equivalente a un $group + $project
+@app.route("/direccion", methods=['GET'])
+def list():
+    mongo_uri = "mongodb://mongo-router:27017"
+
+    client = MongoClient(mongo_uri)
+    db = client.tarea
+    collection = db.direcciones
+
+    pipeline = [{$match: {"ubicacion" :"Mexico"}}, {$count: "ubicacion"}]
+
+    cursor = collection.aggregate(pipeline)
+
+    return cursor
+
+@app.route("/usuarios", methods=['GET'])
+def list():
+    mongo_uri = "mongodb://mongo-router:27017"
+
+    client = MongoClient(mongo_uri)
+    db = client.tarea
+    collection = db.usuarios
+
+    pipeline = [{$match:{"genero":"female", "direccion_id":{$gte: 1000}}}, {$project:{"nombre":1} }, {$sort: {"_id":1}}]
+
+    cursor = collection.aggregate(pipeline)
+
+    return cursor
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
